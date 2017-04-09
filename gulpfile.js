71fc9eb0d3d28081291f9d89afe2cdb6b6e6bdfa
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2017-04-08 19:11:21 -0500

    update bump tasks new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,1 @@
-var gulp = require('gulp');
-var bump = require('./lib/bump');
-
-gulp.task('major', bump.major);
-gulp.task('minor', bump.minor);
-gulp.task('patch', bump.patch);
-gulp.task('alpha', bump.alpha);
+require('./lib');
